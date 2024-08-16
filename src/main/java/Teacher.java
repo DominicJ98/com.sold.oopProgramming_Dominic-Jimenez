637abaa1c48a2c9698d984d32b77d540b6018a21
DominicJ98 <dominic.jimenez@mykenzie.snhu.edu>
2024-08-16 00:12:34 -0700

    Added Super and subclasses

---
+++ src/main/java/Teacher.java
@@ -0,0 +1,14 @@
+public class Faculty extends Person {
+
+    public String name;
+    public String facultyID;
+    public String department;
+    public String title;
+
+    public Faculty(String name, String facultyID, String department, String title) { 
+        this.name = name;
+        this.facultyID = facultyID;
+        this.department = department;
+        this.title = title;
+    }
+}
